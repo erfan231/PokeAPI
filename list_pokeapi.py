255298f8b1a3b79f9eb6b6227d79a174a65ba790
erfan231 <mahmoodierfan7@gmail.com>
2020-09-30 14:03:38 +0800

    updated

--- list_pokeapi.py
+++ list_pokeapi.py
@@ -2,7 +2,7 @@ import urllib
 import json
 from urllib.request import urlopen
 
-site = "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=1"
+site = "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0"
 
 
 user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
@@ -14,7 +14,7 @@ response = urllib.request.urlopen(request)
 datas = json.load(response) 
 
 #if you iterate through a list it starts from 0, blocks user from using index 0
-data_list = ["0"]
+data_list = ['o']
 
 for data in datas["results"]:
     data_list.append(data["name"])
